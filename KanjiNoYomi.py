2063d59702639f14e311a62064525df806b9748a
YL <99618877+IgrecL@users.noreply.github.com>
2022-10-31 20:34:02 +0100

    Cleaner code + custom mode added

--- KanjiNoYomi.py
+++ KanjiNoYomi.py
@@ -1,35 +1,35 @@
 import time, random, os
 from tkinter import *
 
-# add or remove JLPT level to session
-def check(i):
-    if i not in JLPTLevel:
-        JLPTLevel.append(i)
-    else:
-        JLPTLevel.remove(i)
-
 # start session
 def start():
-    if not kanjiList:
-        # delete tkinter widgets in the starting page
-        titleLabel.destroy()
-        N5.destroy()
-        N4.destroy()
-        N3.destroy()
-        N2.destroy()
-        N1.destroy()    
-        N0.destroy()  
-        startButton.destroy()
+    # custom kanji mode
+    if varList[6].get():
+        file = open(str(os.path.dirname(__file__))+'/custom.txt')
+        for line in file.readlines():
+            split = line.split('|')
+            kanjiList.append(line.split('|'))
+    # normal mode
+    else:
+        # add selected JLPT levels
+        for i in range(6):
+            if varList[i].get():
+                JLPTLevel.append(5-i)
         # add selected kanji to kanjiList
         file = open(str(os.path.dirname(__file__))+'/kanji.txt')
         for line in file.readlines():
             split = line.split('|')
             if int(split[1]) in JLPTLevel:
                 kanjiList.append(line.split('|'))
-        global kanjiGoal
-        kanjiGoal = len(kanjiList)
-        # selecting first kanji
-        nextKanji()
+    # delete tkinter widgets
+    for i in range(7):
+        checkbuttonList[i].destroy()
+    titleLabel.destroy()
+    startButton.destroy()
+    global kanjiGoal
+    kanjiGoal = len(kanjiList)
+    # selecting first kanji
+    nextKanji()
  
 # color flash when answering right or wrong
 def changeColor(color):
@@ -67,14 +67,13 @@ def nextKanji():
     furiganaLabel.configure(text='')
     kanjiLabel.configure(text=kanji[0])
     inputField.delete(0,END)
-    progresslabel.configure(text=str(len(kanjiList))+'/'+str(kanjiGoal)+'    ')
+    progresslabel.configure(text=str(kanjiGoal-len(kanjiList))+'/'+str(kanjiGoal)+'    ')
     window.update()
 
 # global variables
 kanji = []
 JLPTLevel = []
 kanjiList = []
-kanjiGoal = 0
 
 # window
 window = Tk()
@@ -86,19 +85,18 @@ window.configure(bg='black')
 # starting page
 titleLabel = Label(window, text='漢字の読み', bg='black', fg='white', font=('Meiryo',30))
 titleLabel.pack(ipady=15)
-checkbuttonArray = []  
-N5 = Checkbutton(window, text='N5', command=lambda: check(5), font=('Helvetica',10,'bold'), bd=5, bg='black', fg='white', activebackground='black', activeforeground='white', selectcolor='black')
-N4 = Checkbutton(window, text='N4', command=lambda: check(4), font=('Helvetica',10,'bold'), bd=5, bg='black', fg='white', activebackground='black', activeforeground='white', selectcolor='black')
-N3 = Checkbutton(window, text='N3', command=lambda: check(3), font=('Helvetica',10,'bold'), bd=5, bg='black', fg='white', activebackground='black', activeforeground='white', selectcolor='black')
-N2 = Checkbutton(window, text='N2', command=lambda: check(2), font=('Helvetica',10,'bold'), bd=5, bg='black', fg='white', activebackground='black', activeforeground='white', selectcolor='black')
-N1 = Checkbutton(window, text='N1', command=lambda: check(1), font=('Helvetica',10,'bold'), bd=5, bg='black', fg='white', activebackground='black', activeforeground='white', selectcolor='black')
-N0 = Checkbutton(window, text='N0', command=lambda: check(0), font=('Helvetica',10,'bold'), bd=5, bg='black', fg='white', activebackground='black', activeforeground='white', selectcolor='black')
-N5.pack(pady=2)
-N4.pack(pady=2)
-N3.pack(pady=2)
-N2.pack(pady=2)  
-N1.pack(pady=2)
-N0.pack(pady=2)
+varList = []
+checkbuttonList = []  
+for i in range(7):
+    if i<=5:
+        txt = 'JLPT N'+str(5-i)
+    else:
+        txt = 'custom'
+    var = StringVar(window)
+    cb = Checkbutton(window, text=txt, variable=var, width=7, font=('Helvetica',10,'bold'), bd=5, bg='black', fg='white', activebackground='black', activeforeground='white', selectcolor='black')
+    cb.pack(pady=2)
+    checkbuttonList.append(cb)
+    varList.append(var)
 startButton = Button(window, command=start, text='START', width=19, bg='black', fg='white', activebackground='black', activeforeground='white', font=('Helvetica',15,'bold'))
 startButton.pack(ipady=5, pady=30)
 
